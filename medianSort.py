66ce35071a684333c4cd511c40be52a46c38872b
Luis Fernando Manzano Alvarez <luis.m@rivka.mx>
2022-07-16 11:49:51 -0400

    completed first challenge

---
+++ medianSort.py
@@ -0,0 +1,49 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+def median_sort(array):
+    highest_median = 0
+    index_to_sort = 0
+
+    for idx, element in enumerate(array):
+        if isinstance(element, list):
+            avg = sum(element) / len(element)
+            element.append(avg)
+        else:
+            avg = element
+            array[idx] = [element, avg]
+
+        if avg > highest_median:
+            highest_median = avg
+            index_to_sort = idx
+
+    sorted_array = array[index_to_sort:]
+    sorted_array.sort(key=lambda median: median[-1])
+
+    if index_to_sort > 0:
+        sorted_array = array[0:index_to_sort] + sorted_array
+        sorted_array = remove_means(sorted_array)
+        return sorted_array
+
+    sorted_array = remove_means(sorted_array)
+    return sorted_array
+
+
+def remove_means(array):
+    for element in array:
+        element.pop()
+    return array
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test1 = [[2.4, 0.2, 9.8], 0, [-1], [-9, -4]]
+
+    print(median_sort(test1))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
